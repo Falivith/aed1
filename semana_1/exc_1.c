81e338fc4c238edead3423c15aa1eb61d60d16b1
Gabriel Ramires <falivith@gmail.com>
2022-03-12 03:40:03 -0300

    Implementada condição que não aceita tirar meias palavras do meio da string, agora a função que remove nome verifica se o nome passado realmente está completo antes de remover.

--- semana_1/exc_1.c
+++ semana_1/exc_1.c
@@ -9,13 +9,14 @@
 
 char* addnome(char* str);
 char* remnome(char* str);
+//char* findword(char* str);
 
 int main(){
 SetConsoleOutputCP(65001);
 
 int exit = 1, controle, case_loop = 1;
 char *string = calloc(2 ,sizeof(char));
-strcat(string, "|\0");
+strcat(string, "|\0"); //inicializa a string com 1 separador e um caractere nulo
 
 while(exit){
     printf("\n1) Adicionar nomes \n");
@@ -63,20 +64,17 @@ while(exit){
     }
 }
 
-free(string);
 return 0;
 }
 
 char* addnome(char* str){
-    int str_length, buffer_length;
-    char div[] = "|";
-    char temp_str[25];
+    int buffer_length;
+    char div[] = "|", temp_str[25];
     printf("\nInsira o nome: ");
     scanf("%s", temp_str);
     getc(stdin);
     strcat(temp_str, div); //adiciona o marcador
     buffer_length = strlen(temp_str);
-    str_length = strlen(str);
     realloc(str, sizeof(str)+sizeof(char)*buffer_length); //soma a memória já alocada com o tamanho do nome
     strcat(str, temp_str); //adiciona o nome
     return str;
@@ -84,21 +82,24 @@ char* addnome(char* str){
 
 char* remnome(char* str){
     int tamanho, i;
-    char buffer[50];
-    char div[] = "|";
-    char *start, *end; 
+    char buffer[50], div[] = "|";
+    char *start, *end, *test; 
+
+    inicio: 
     printf("\nInsira o nome a ser removido da lista: ");
     scanf("%s", buffer);
     getc(stdin);
-
-
-
     strcat(buffer, div);
-    tamanho = strlen(buffer);
-    start = strstr(str, buffer); //achado o ponteiro pro começo da palavra a ser retirada, ou Null caso ela n exista
-    end = start;  //guarda o local do começo da palavra a ser retirada
+    test = strstr(str, buffer);
+
+    if(test !=  NULL){ 
+        tamanho = strlen(buffer);
+        start = strstr(str, buffer); //achado o ponteiro pro começo da palavra a ser retirada, ou Null caso ela n exista
+        end = start;  //guarda o local do começo da palavra a ser retirada
         for(i=0; i<tamanho; i++)
-        end++; //end agora aponta pra primeira letra da proxima palavra 
-    memmove(start, end, strlen(end)+1); //sobrescreve a palavra com o restante da string. o +1 é importantissimo no memmove, para trazer o caractere nulo também. 
-    return str; 
+            end++; //end agora aponta pra primeira letra da proxima palavra 
+        memmove(start, end, strlen(end)+1); //sobrescreve a palavra com o restante da string. o +1 é para trazer o caractere nulo também. 
+            return str; 
+        }
+        else{goto inicio;}
 }
