fd5b34394d125aa3e07dcc96a472705bf5aa890d
Gabriel Ramires <falivith@gmail.com>
2022-03-18 14:36:31 -0300

    Construção da func add_cadastro

--- semana_2/exc_3.c
+++ semana_2/exc_3.c
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct dados{ 
+/*struct dados{ 
     int idade;
     int telefone;
     char nome[10];
-};
+};*/
 
 void add_cadastro(void *pBuffer){
-    char temp[50];
-    printf("\nDigite o nome do cliente.");
-    scanf("%s", temp);
+    void *p = pBuffer;
+    unsigned int temp_idade, i;
+    char temp_nome[11], telefone[9];
+    printf("\n\nDigite o nome do cliente: ");
+    fgets(temp_nome, sizeof(temp_nome), stdin);
+    printf("\nDigite a idade do cliente: ");
+    scanf("%d", &temp_idade);   getc(stdin);
+
+        printf("\nDigite o telefone do cliente: ");
+        fgets(telefone, sizeof(telefone), stdin);
+        
+
     }
 
 int main(){
     int sair = 1; 
     int option = 1;
-    //int clientes = 0;
-    void *pBuffer = calloc(1,1);
+    void *pBuffer = calloc(1,sizeof(int)); //é reservado um int pra guardar o Npessoas
 
 
     while(sair){
@@ -30,25 +38,24 @@ int main(){
         printf("\n5) Sair"); 
         scanf("%d", &option);
 
-            switch(option)
-            {
-            case 1: 
-                printf("\n");
-                add_cadastro(pBuffer);
-            break;
+        switch(option)
+        {
+        case 1: 
+            add_cadastro(pBuffer);
+        break;
 
-            case 2:
-            break;
+        case 2:
+        break;
 
-            case 3: 
-            break;
+        case 3: 
+        break;
 
-            case 4:
-                sair = 0; 
-            break; 
+         case 4:
+            sair = 0; 
+        break; 
 
-            default:
-            break;
+        default:
+        break;
         }
     }
 
