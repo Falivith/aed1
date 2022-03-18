8482ab65b938485223a8e00321bbf041e728601b
Gabriel Ramires <falivith@gmail.com>
2022-03-18 16:26:00 -0300

    nem tentei compilar ainda mas to salvando

--- semana_2/exc_3.c
+++ semana_2/exc_3.c
@@ -10,17 +10,35 @@
 
 void add_cadastro(void *pBuffer){
     void *p = pBuffer;
-    unsigned int temp_idade, i;
+    int temp_idade, tel_size = 0, n_cliente, i, size_dados;
     char temp_nome[11], telefone[9];
+
     printf("\n\nDigite o nome do cliente: ");
     fgets(temp_nome, sizeof(temp_nome), stdin);
     printf("\nDigite a idade do cliente: ");
     scanf("%d", &temp_idade);   getc(stdin);
 
-        printf("\nDigite o telefone do cliente: ");
+        while(tel_size =! 8){
+        printf("\n(8 caracteres) Digite o telefone do cliente: ");
         fgets(telefone, sizeof(telefone), stdin);
-        
+        tel_size = strlen(telefone);
+        }
 
+    n_cliente = (int*) pBuffer;
+    size_dados = sizeof(int) + n_cliente * (sizeof(temp_nome) + sizeof(telefone) + sizeof(temp_idade) );
+
+    pBuffer = realloc(pBuffer, sizeof(int)+size_dados); 
+
+        p = p + size_dados;               //pula o numero de clientes e vai para o campo nome
+            memset(p, '\0', sizeof(temp_nome));  
+            strcat(p, temp_nome);               //preenche o campo nome
+        p = p + sizeof(temp_nome);      //pula o tamanho do nome e vai para o campo idade
+            p = temp_idade;                     //preenche a idade
+        p = p + sizeof(int);            //pula o tamanho da idade e vai para o campo telefone
+            memset(p, '\0', sizeof(telefone)); 
+            strcat(p, telefone);                //preenche o telefone
+
+    *pBuffer++; //o conteudo do endereço 0 é um int, logo, incrementar pBuffer aumenta o n de clientes
     }
 
 int main(){
@@ -28,31 +46,30 @@ int main(){
     int option = 1;
     void *pBuffer = calloc(1,sizeof(int)); //é reservado um int pra guardar o Npessoas
 
-
     while(sair){
 
         printf("\n1) Incluir novo cadastro");
         printf("\n2) Excluir cadastro");
         printf("\n3) Buscar cliente");
         printf("\n4) Listar clientes");
-        printf("\n5) Sair"); 
+        printf("\n5) Sair");
         scanf("%d", &option);
 
         switch(option)
         {
-        case 1: 
+        case 1:
             add_cadastro(pBuffer);
         break;
 
         case 2:
         break;
 
-        case 3: 
+        case 3:
         break;
 
          case 4:
-            sair = 0; 
-        break; 
+            sair = 0;
+        break;
 
         default:
         break;
