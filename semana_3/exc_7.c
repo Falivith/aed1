0a6e55fdc8e4bd2b3d9fe9bcbcf46305d7238eda
Gabriel Ramires <falivith@gmail.com>
2022-03-28 03:46:30 -0300

    Faltando remoção de cliente exc7

--- semana_3/exc_7.c
+++ semana_3/exc_7.c
@@ -4,39 +4,51 @@
 
 #include <windows.h> //apenas pra acentuação em conjunto com a primeira linha do main
 
+#define QNT_PESSOAS 10
+
 typedef struct data{
 char nome[11];
 char telefone[9];
 int idade;
 }Pessoa; 
 
-Pessoa pessoas[10];
+Pessoa pessoas[QNT_PESSOAS];
 
 void* menu_agenda(void *pBuffer);
 void* add_cadastro(void *pBuffer);
-/*void* listar_cadastro(void *pBuffer);
-void* buscar_cadastro(void *pBuffer);
-void* rem_cadastro(void *pBuffer);*/
+void* listar_cadastro(void *pBuffer);
+//void* buscar_cadastro(void *pBuffer);
+void* rem_cadastro(void *pBuffer);
 
 
 
 int main(){
-void* pBuffer = malloc(4*sizeof(int));            SetConsoleOutputCP(65001); //acentuação          
-*(int*)pBuffer = 1;                               //1º Campo INT de pBuffer = controle do while do menu
-void* ptr = pBuffer + 2*sizeof(int); 
-*(int*)ptr = 0;                                   //3º Campo de pBuffer = Contagem de Usuarios preenchidos  
+    SetConsoleOutputCP(65001); //acentuação         
+    void* pBuffer = malloc(5*sizeof(int));            //armazena espaço para as variáveis do programa
+
+    *(int*)pBuffer = 1;                               //1º Campo INT de pBuffer = controle do while do menu
+    void* ptr = pBuffer + sizeof(int);                
+    *(int*)ptr = 0;                                   //2º Campo INT de pBuffer = controle do CASE do menu 
+    ptr = pBuffer + 2*sizeof(int); 
+    *(int*)ptr = 0;                                   //3º Campo INT de pBuffer = Contagem de Usuarios preenchidos  
+    ptr = pBuffer + 3*sizeof(int);
+    *(int*)ptr = 0;                                   //4º Campo INT de pBuffer = Inteiro usado pra teste na função add_cadastro
+    ptr = pBuffer + 4*sizeof(int);              
+    *(int*)ptr = 0;                                   //5º Campo INT de pBuffer = I usado para contadores FOR, utilitário e afins
+
 
     while(*(int*)pBuffer){                   
-    menu_agenda(pBuffer);
+        menu_agenda(pBuffer);
     }
 
 return 0;
 }
 
+    //\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\//
+
 void* menu_agenda(void *pBuffer){
 
-    void *ptr = pBuffer; 
-    ptr = ptr + sizeof(int);                      //2º Campo INT de pBuffer = controle do CASE do menu 
+    void* ptr = pBuffer + sizeof(int);                //Acessando o 2º campo (controle do CASE)
 
     printf("\n1) Incluir novo cadastro");
     printf("\n2) Excluir cadastro");
@@ -55,7 +67,7 @@ void* menu_agenda(void *pBuffer){
         break;
 
         case 2:
-            //pBuffer = rem_cadastro(pBuffer);
+            pBuffer = rem_cadastro(pBuffer);
         break;
 
         case 3:
@@ -63,7 +75,7 @@ void* menu_agenda(void *pBuffer){
         break;
 
         case 4:
-            //pBuffer = listar_cadastro(pBuffer);
+            pBuffer = listar_cadastro(pBuffer);
         break;
 
         case 5:
@@ -77,41 +89,73 @@ void* menu_agenda(void *pBuffer){
 return pBuffer;
 }
 
+    //\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\//
+
 void* add_cadastro(void *pBuffer){
 
-    void *ptr_1 = pBuffer, *ptr = pBuffer;
-    ptr = ptr + 2*sizeof(int);             //3º Campo de pBuffer = Contagem de Usuarios preenchidos  
-    ptr_1 = ptr_1 + 3*sizeof(int);         //4º Campo de pBuffer = Inteiro usado pra teste no preenchimento de telefone
-    *(int*)ptr_1 = 0; 
+    void *n_users = pBuffer + 2*sizeof(int);  //3º Campo de pBuffer
+    void *teste = pBuffer + 3*sizeof(int);  //4º Campo de pBuffer
+
+        if(*(int*)n_users+1 > QNT_PESSOAS){
+            printf("\nNão há espaço para mais usuários! Remova algum ou saia do programa.\n");
+            return pBuffer;
+        }                     
 
     printf("\nDigite o nome (10 Caracteres) do cliente: ");
-    fgets(pessoas[*(int*)ptr].nome, sizeof(pessoas->nome), stdin);
-    pessoas[*(int*)ptr].nome[strcspn(pessoas[*(int*)ptr].nome, "\n")] = 0;
+    fgets(pessoas[*(int*)n_users].nome, sizeof(pessoas->nome), stdin);
+    pessoas[*(int*)n_users].nome[strcspn(pessoas[*(int*)n_users].nome, "\n")] = 0;
 
     printf("Digite a idade do cliente: ");
-    scanf("%d", pessoas[*(int*)ptr].idade);   getc(stdin);
+    scanf("%d", &pessoas[*(int*)n_users].idade);   getc(stdin);
 
-        while(*(int*)ptr_1 != 8){
-        printf("(8 caracteres, mais serão ignorados) Digite o telefone do cliente: ");
-        fgets(pessoas[*(int*)ptr].telefone, sizeof(pessoas->telefone), stdin);
-        *(int*)ptr_1 = strlen(pessoas[*(int*)ptr].telefone);
+        while(*(int*)teste != 8){
+        printf("Digite o telefone do cliente: ");
+        fgets(pessoas[*(int*)n_users].telefone, sizeof(pessoas->telefone), stdin);
+        *(int*)teste = strlen(pessoas[*(int*)n_users].telefone);
+        }
+        
+        *(int*)teste = 0; 
+        *(int*)n_users = *(int*)n_users+1; 
+
+    return pBuffer;
+    }
+
+    //\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\////\\//\\//\\//
+
+    void* listar_cadastro(void *pBuffer){
+
+        void* n_users = pBuffer + 2*sizeof(int);            //Campo 3: numero de usuarios
+        void* contador = pBuffer + 4*sizeof(int);           //Campo 5: contador FOR
+
+        for(*(int*)contador = 0; *(int*)contador < *(int*)n_users; *(int*)contador = *(int*)contador+1){
+            printf("\nCliente Nº %d:", *(int*)contador+1);
+            printf("\nNome: %s", pessoas[*(int*)contador].nome);
+            printf("\nIdade: %d", pessoas[*(int*)contador].idade); 
+            printf("\nTelefone: %s", pessoas[*(int*)contador].telefone);
+            printf("\n\n");
+        }
+        return pBuffer; 
+    }
+
+    void* rem_cadastro(void *pBuffer){
+
+        void* rem_user = pBuffer + 4*sizeof(int);           //Campo 5: Utilitário para while/for
+        *(int*)rem_user = 1; 
+        void* n_users = pBuffer + 2*sizeof(int);            //Campo 3: numero de usuarios
+
+        while(*(int*)rem_user>0 && *(int*)rem_user<QNT_PESSOAS){
+            printf("Digite o número (0 para voltar ao menu) do cliente que deseja remover da lista: ");
+            scanf("%d", (int*)rem_user);     getc(stdin);
+                if(*(int*)rem_user == 0){
+                    printf("\nVoltando ao menu...");
+                    return pBuffer; 
+                }
         }
 
-    /*||||||||||||| passagem dos dados pro pBuffer |||||||||||||*/
+        
 
-    n_cliente = *(int*)pBuffer; 
-    size_dados = (sizeof(temp_nome) + sizeof(telefone) + sizeof(int));  //calcula a quantidade de memória necessária pro cadastro
-    pBuffer = realloc(pBuffer, sizeof(int) + n_cliente*size_dados); 
-    p = pBuffer; 
 
-        pBuffer = pBuffer + sizeof(int);                             //pula o campo n_clientes e vai para o campo nome
-        pBuffer = pBuffer + (n_cliente-1) * size_dados;              //pula a quantidade de dados já usada por outros cadastros
-            memmove((char*)pBuffer, temp_nome, sizeof(temp_nome));   //preenche o campo nome
-        pBuffer = pBuffer + sizeof(temp_nome);                       //pula o tamanho do nome e vai para o campo idade
-            *(int*)pBuffer = temp_idade;                             //preenche a idade
-        pBuffer = pBuffer + sizeof(int);                             //pula o tamanho da idade e vai para o campo telefone
-            memmove((char*)pBuffer, telefone, sizeof(telefone));     //prenche o telefone
 
-    *(int*)ptr++; 
-    return pBuffer;
+        
+    return pBuffer; 
     }
